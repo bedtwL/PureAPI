8ac505bc1e4c26f18b8e2fdf5fc87d5294cf165e
bedtwL <blueaustin0507@gmail.com>
2025-06-22 15:14:53 +0800

    1.0.1-HOTFIX

--- src/main/java/me/bedtwL/ffa/api/EffectAddon.java
+++ src/main/java/me/bedtwL/ffa/api/EffectAddon.java
@@ -5,7 +5,9 @@ import java.util.UUID;
 public interface EffectAddon {
     String getName();
     String getAuthor();
-    UUID authorUUID = null;
+    default UUID authorUUID() {
+        return UUID.fromString("f6edfcd8-f6b8-4e57-9367-8648953eaae2");
+    }
     void onEnable();
     default void onFirstLoad() {
     }
